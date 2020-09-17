757f7518f3d2976396de833d70bb3ae99cbaab02
danymessi1000@gmail.com <danymessi1000@gmail.com>
2020-09-17 12:14:03 -0500

    Agregue prints y la grafica

--- Codigo.py
+++ Codigo.py
@@ -9,9 +9,10 @@ class Variables():
 
     def Calcular(self):
         Cm=(self.__pv-self.__cv)
-        Cu=(self.__pv/Cm)
+        Cu=(self.__pv-Cm)
+        va=(Cm/100)
         piezas=(self.__cf/Cm)
-        Cf_Mcu=(self.__cf/Cu)
+        Cf_Mcu=(self.__cf/va)
         comprobacion=(piezas*self.__pv)
         print("Calculando Respuestas...")
         time.sleep(5)
@@ -21,7 +22,7 @@ class Variables():
         print(f"Precio de Venta Unitario:{self.__pv}")
         print(f"Costos Variables :{self.__cv}")
         print(f"Contribucion Marginal :{Cm}")
-        print(f"% Contribucion Unitario : {Cu}")
+        print(f"% Contribucion Unitario : %{Cu}")
         print(f"Costos Fijos :{self.__cf}")
         print("*"*40)
         print("PE Uds")
@@ -29,11 +30,19 @@ class Variables():
         print("*"*40)
         print("PE Ingresos")
         print(f"CF/%MCU :{Cf_Mcu}")
+        print(f"Comprobacion : {comprobacion}")
         print("*"*50)
 
         print("Graficando Grafica...")
         time.sleep(5)
-        
+
+        nombres=["Precio_Venta","Costo_Variable","Costo_Fijos","Contribucion_Marginal","CF/MCU","CF/%MCU","Comprobacion"]
+        valores=[self.__pv,self.__cv,self.__cf,Cm,piezas,Cf_Mcu,comprobacion]
+        colores=["red","blue","green","pink","yellow","purple","orange"]
+        plt.title("Grafica de Variables")
+        plt.bar(nombres,height=valores,color=colores,width=0.5)
+        plt.show()
+
 
 
 
