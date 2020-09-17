6e04b1a99a424de7adef880d86eafebe2e7ba515
danymessi1000@gmail.com <danymessi1000@gmail.com>
2020-09-16 22:00:43 -0500

    Agregue la funcion y unas variables

--- Codigo.py
+++ Codigo.py
@@ -6,3 +6,10 @@ class Variables():
         self.__pv=pv
         self.__cv=cv
         self.__cf=cf
+
+    def Calcular(self):
+        Cm=(self.__pv-self.__cv)
+        Cu=(self.__pv/Cm)
+        piezas=(self.__cf/Cm)
+        Cf_%Mcu=(self.__cf/Cu)
+        comprobacion=(piezas*self.__pv)
